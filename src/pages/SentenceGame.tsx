766c9a382ee4049b575c654b573a50cf6dad1190
Rohan Babbar <rohanbabbar2003@gmail.com>
2025-04-16 11:52:45 +0530

    Update SentenceGame.tsx

--- src/pages/SentenceGame.tsx
+++ src/pages/SentenceGame.tsx
@@ -8,12 +8,12 @@ import { useLocalStorage } from "../hooks/useLocalStorage";
 import { IoExitOutline } from "react-icons/io5";
 
 const SentenceGame = () => {
-  const [_questions, setQuestions] = useLocalStorage<Question[]>(
+  const [questions, setQuestions] = useLocalStorage<Question[]>(
     "questions",
     []
   );
-  const [_index, setIndex] = useLocalStorage("index", 0);
-  const [_answers, setAnswers] = useLocalStorage<string[][]>("answers", []);
+  const [index, setIndex] = useLocalStorage("index", 0);
+  const [answers, setAnswers] = useLocalStorage<string[][]>("answers", []);
   const [selected, setSelected] = useState<(string | null)[]>([]);
   const [resetSignal, setResetSignal] = useState(0);
   const navigate = useNavigate();
